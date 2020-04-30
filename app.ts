ae42448112d35545c74c524782b74f9016a42722
Nuria <c.odena.nuria@gmail.com>
2020-04-30 16:57:11 +0200

    testing

--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
-console.log("Hi from ts")
+function add (n1: number, n2: number, showResult: boolean, phrase: string) {
+    console.log(typeof number2)
+    const result = n1 + n2;
+    if (showResult){
+        console.log( phrase + result)
+    } else {
+        return result
+    }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase =  'Result is: ';
+add(number1, number2, printResult, resultPhrase);
+
