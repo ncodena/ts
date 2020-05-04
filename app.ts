e5ae3b4b7ea0a7b1bc498993a14bfb593fe2a1cc
Nuria <c.odena.nuria@gmail.com>
2020-05-04 11:34:20 +0200

    testing code

--- app.ts
+++ app.ts
@@ -1,10 +1,19 @@
-function combine (input1: number, input2: number) {
-    const result = input1 + input2;
+// Union type => it accepts as many types as needed
 
+function combine (input1: number | string, input2: number | string) {
+    let result;
+    if(typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    }
+    
     return result;
     
 }
 
 const combinedAges = combine(30,26);
+const combinedNames = combine('Brad', 'Anna')
 
 console.log(combinedAges)
+console.log(combinedNames)
