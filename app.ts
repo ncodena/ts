32610837652cbb0f66b31fad1bcbee97eb43484a
Nuria <c.odena.nuria@gmail.com>
2020-05-04 11:23:20 +0200

    testing code

---
+++ app.ts
@@ -0,0 +1,10 @@
+function combine (input1: number, input2: number) {
+    const result = input1 + input2;
+
+    return result;
+    
+}
+
+const combinedAges = combine(30,26);
+
+console.log(combinedAges)
